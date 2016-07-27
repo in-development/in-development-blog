443de7a2f5c20821712dfad7b93c181a4ce68926
Rodrigo Morais <morais.rm@gmail.com>
2016-07-26 21:09:32 -0300

    Creating a layout to new Post without style.

--- src/Post/New/View.elm
+++ src/Post/New/View.elm
@@ -1,10 +1,40 @@
 module Post.New.View exposing (..)
 
-import Html exposing (div, text)
+import Html exposing (div, text, label, input, textarea, button)
+import Html.Attributes exposing (style)
 
 
 import Post.New.Messages exposing (..)
 
 newPostView : Html.Html Msg
 newPostView = div []
-              [text "TEST"]
+                  [
+                    div [ style [("margin", "1em")] ]
+                        [
+                          label [ style [("display", "block"), ("clear", "right")] ]
+                                [ text "Author:"],
+                          input [ style [("width", "30%")] ]
+                                []
+                        ],
+                    div [ style [("margin", "1em")] ]
+                        [
+                          label [ style [("display", "block"), ("clear", "right")] ]
+                                [ text "Post:"],
+                          textarea [ style [("width", "100%"), ("height", "40%"), ("min-height", "400px")] ]
+                                   []
+                        ],
+                    div [ style [("margin", "1em")] ]
+                        [
+                          button [ style [
+                                          ("position", "relative"),
+                                          ("float", "right"),
+                                          ("width", "10%"),
+                                          ("height", "10%"),
+                                          ("min-height", "30px"),
+                                          ("font-size", "1em"),
+                                          ("cursor", "pointer")
+                                         ]
+                                 ]
+                                 [ text "Save" ]
+                        ]
+                  ]

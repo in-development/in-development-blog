e7798a9780e82b61db1efaa33834293a0f529d53
Vinicius Fernandes <viniciusffj@gmail.com>
2016-09-04 19:44:55 -0300

    Adds tests for url parser
    Closes #28.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,20 +1,12 @@
 module Tests exposing (..)
 
 import Test exposing (..)
-import Expect
-import String
+
+import RoutingParser
 
 
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
-            \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+    describe "In development"
+        [ RoutingParser.all
         ]

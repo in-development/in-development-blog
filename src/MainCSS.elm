21e3490cecd5978d664bc30e2c627beaa49a8dea
Rodrigo Morais <morais.rm@gmail.com>
2016-09-06 18:53:31 -0300

    Creating the stylesheets to remove the margin from body.

---
+++ src/MainCSS.elm
@@ -0,0 +1,13 @@
+module MainCSS exposing (css)
+
+
+import Css exposing (..)
+import Css.Elements exposing (body)
+import Css.Namespace exposing (namespace)
+
+
+css =
+    (stylesheet << namespace "dreamwriter")
+        [ body
+            [ margin zero ]
+        ]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('./assets/markdown.css');
+require('./assets/styles.css');
 
 // Require index.html so it gets copied to dist
 require('./index.html');

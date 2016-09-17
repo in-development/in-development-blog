23f1e0004af86fe8f281a61730ca0921535fec04
Rodrigo Morais <morais.rm@gmail.com>
2016-09-16 23:13:32 -0300

    Refactoring the view of show post.

--- src/Post/Show/View.elm
+++ src/Post/Show/View.elm
@@ -17,34 +17,16 @@ import Base64
 
 postView : Post -> Html.Html Msg
 postView post =
-  let
-    postId = "posts-" ++ (toString post.id)
-
-    postTextStr =
-      case (Base64.decode post.text) of
-        Ok text -> text
-        Err _ -> ""
-
-  in
-    div [ style [("width", "70%"), ("left", "10%"), ("position", "relative"), ("float", "left"), ("margin-bottom", "0.5em")] ]
-        [ div [ style [("margin", "0.5em 0 0 0.5em")]]
-              [
-                postText postTextStr True
-              ]
-        , div [style [("text-align", "right"), ("margin", "0.5em 0.5em 0.5em 0"), ("font-size", "0.8em")]]
-              [ text post.author ]
-        ]
+  div [ style [("width", "70%"), ("left", "10%"), ("position", "relative"), ("float", "left"), ("margin-bottom", "0.5em")] ]
+    [ postBody post True
+    , postAuthor post
+    ]
 
 
 postSummary : Post -> Html.Html Msg
 postSummary post =
   let
     postId = "posts-" ++ (toString post.id)
-
-    postTextStr =
-      case (Base64.decode post.text) of
-        Ok text -> text
-        Err _ -> ""
 
   in
     a [ id postId
@@ -54,34 +36,59 @@ postSummary post =
       ]
       [
         div [ style [("width", "70%"), ("left", "10%"), ("position", "relative"), ("float", "left"), ("margin-bottom", "0.5em")] ]
-            [ div [ style [("margin", "0.5em 0 0 0.5em")]]
-                  [
-                    postText postTextStr False
-                  ]
-            , div [style [("text-align", "right"), ("margin", "0.5em 0.5em 0.5em 0"), ("font-size", "0.8em")]]
-                  [ text post.author ]
-            , hr [ style [ ("border", "0")
-                         , ("height", "2px")
-                         , ("border-top", "1px dashed #000000")
-                         , ("border-bottom", "1px dashed black")
-                         , ("margin-top", "4.5em")
-                         ]
-                 ]
-                 [ ]
+            [ postBody post False
+            , postAuthor post
+            , lineBreak
             ]
       ]
+
+
+postBody : Post -> Bool -> Html.Html Msg
+postBody post complete =
+  let
+    postTextStr =
+      case (Base64.decode post.text) of
+        Ok text -> text
+        Err _ -> ""
+
+  in
+    div [ style [("margin", "0.5em 0 0 0.5em")]]
+        [
+          postText postTextStr complete
+        ]
 
 
 postText : String -> Bool -> Html.Html Msg
 postText text complete =
   let
+    lineBreakText =
+      "-----<<<<<continue<<<<<-----"
+
     newText =
       if complete then
-        Just (join "\n" (List.map (\line -> if line == "-----<<<<<continue<<<<<-----" then "" else line) (lines text)))
+        Just (join "\n" (List.map (\line -> if line == lineBreakText then "" else line) (lines text)))
       else
-        List.head (split "-----<<<<<continue<<<<<-----" text)
+        List.head (split lineBreakText text)
 
   in
     case newText of
       Just text' -> Markdown.toHtml [class "content hlsj"] text'
       Nothing -> Markdown.toHtml [class "content hlsj"] ""
+
+
+postAuthor : Post -> Html.Html Msg
+postAuthor post =
+  div [style [("text-align", "right"), ("margin", "0.5em 0.5em 0.5em 0"), ("font-size", "0.8em")]]
+      [ text post.author ]
+
+
+lineBreak : Html.Html Msg
+lineBreak =
+  hr [ style [ ("border", "0")
+             , ("height", "2px")
+             , ("border-top", "1px dashed #000000")
+             , ("border-bottom", "1px dashed black")
+             , ("margin-top", "4.5em")
+             ]
+     ]
+     [ ]

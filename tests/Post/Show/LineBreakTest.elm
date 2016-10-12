469d6b8b366abd74e2701829871ee362ffba7659
Rodrigo Morais <morais.rm@gmail.com>
2016-10-11 22:15:40 -0300

    Updating tests to line break organizing for describers.

--- tests/Post/Show/LineBreakTest.elm
+++ tests/Post/Show/LineBreakTest.elm
@@ -14,12 +14,14 @@ import Post.Show.View exposing (..)
 all: Test
 all =
   describe "Line Break"
-    [ test "Should return just the content the post above line break" <|
-      \() ->
-        Expect.equal Fixtures.postHeadLineBreak (postHeadLineBreak Fixtures.textWithLineBreakMark)
-    , test "Should return all post when doesn't have line break" <|
-      \() ->
-        Expect.equal Fixtures.postWithoutLineBreakMark (postHeadLineBreak Fixtures.textWithoutLineBreakMark)
+    [ describe "postHeadLineBreakMark"
+        [ test "Should return just the content the post above line break" <|
+          \() ->
+            Expect.equal Fixtures.postHeadLineBreak (postHeadLineBreak Fixtures.textWithLineBreakMark)
+        , test "Should return all post when doesn't have line break" <|
+          \() ->
+            Expect.equal Fixtures.postWithoutLineBreakMark (postHeadLineBreak Fixtures.textWithoutLineBreakMark)
+        ]
     , describe "postHasLineBreakMarking"
         [ test "Should return True when text has a line break mark" <|
           \() ->

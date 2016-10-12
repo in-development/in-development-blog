7f56d5db731b0163a4b6195f6af695b2a423b22a
Rodrigo Morais <morais.rm@gmail.com>
2016-10-11 22:18:52 -0300

    Adding the line breaks tests to the execution.

--- tests/Post/Show/ViewTest.elm
+++ tests/Post/Show/ViewTest.elm
@@ -5,11 +5,13 @@ import Test exposing (..)
 import Expect
 
 
-import Post.Show.AuthorTest as PostShowAuthor 
+import Post.Show.AuthorTest as PostShowAuthor
+import Post.Show.LineBreakTest as PostShowLineBreak
 
 
 all: Test
 all =
   describe "Post View Show"
     [ PostShowAuthor.all
+    , PostShowLineBreak.all
     ]

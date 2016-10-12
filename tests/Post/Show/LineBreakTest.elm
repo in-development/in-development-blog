521b80661370ee02af63d98b44c93cf5a354d048
Rodrigo Morais <morais.rm@gmail.com>
2016-10-11 22:13:00 -0300

    Adding test to function "postHasLineBreakMarking" in Post.Show.View.

--- tests/Post/Show/LineBreakTest.elm
+++ tests/Post/Show/LineBreakTest.elm
@@ -20,4 +20,12 @@ all =
     , test "Should return all post when doesn't have line break" <|
       \() ->
         Expect.equal Fixtures.postWithoutLineBreakMark (postHeadLineBreak Fixtures.textWithoutLineBreakMark)
+    , describe "postHasLineBreakMarking"
+        [ test "Should return True when text has a line break mark" <|
+          \() ->
+           Expect.true "Expected True when text has a line break mark" (postHasLineBreakMarking Fixtures.textWithLineBreakMark)
+        , test "Should return False when text hasn't a line break mark" <|
+          \() ->
+            Expect.false "Expected False when text hasn't a line brak mark" (postHasLineBreakMarking Fixtures.textWithoutLineBreakMark)
+        ]
     ]

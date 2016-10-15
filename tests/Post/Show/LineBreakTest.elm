fed6f9840bc055977ed663dd58d00ec42f050a7f
Rodrigo Morais <morais.rm@gmail.com>
2016-10-15 12:02:17 -0300

    Removing nested describers in tests and creating a function to each desbribe.

--- tests/Post/Show/LineBreakTest.elm
+++ tests/Post/Show/LineBreakTest.elm
@@ -11,23 +11,33 @@ import Post.Show.Fixtures as Fixtures
 import Post.Show.View exposing (..)
 
 
-all: Test
+all : Test
 all =
   describe "Line Break"
-    [ describe "postHeadLineBreakMark"
-        [ test "Should return just the content the post above line break" <|
-          \() ->
-            Expect.equal Fixtures.postHeadLineBreak (postHeadLineBreak Fixtures.textWithLineBreakMark)
-        , test "Should return all post when doesn't have line break" <|
-          \() ->
-            Expect.equal Fixtures.postWithoutLineBreakMark (postHeadLineBreak Fixtures.textWithoutLineBreakMark)
-        ]
-    , describe "postHasLineBreakMarking"
-        [ test "Should return True when text has a line break mark" <|
-          \() ->
-           Expect.true "Expected True when text has a line break mark" (postHasLineBreakMarking Fixtures.textWithLineBreakMark)
-        , test "Should return False when text hasn't a line break mark" <|
-          \() ->
-            Expect.false "Expected False when text hasn't a line brak mark" (postHasLineBreakMarking Fixtures.textWithoutLineBreakMark)
-        ]
+    [ postHeadLineBreakMarkTest
+    , postHasLineBreakMarkingTest
+    ]
+
+
+postHeadLineBreakMarkTest : Test
+postHeadLineBreakMarkTest =
+  describe "postHeadLineBreakMark"
+    [ test "Should return just the content the post above line break" <|
+      \() ->
+        Expect.equal Fixtures.postHeadLineBreak (postHeadLineBreak Fixtures.textWithLineBreakMark)
+    , test "Should return all post when doesn't have line break" <|
+      \() ->
+        Expect.equal Fixtures.postWithoutLineBreakMark (postHeadLineBreak Fixtures.textWithoutLineBreakMark)
+    ]
+
+
+postHasLineBreakMarkingTest : Test
+postHasLineBreakMarkingTest =
+  describe "postHasLineBreakMarking"
+    [ test "Should return True when text has a line break mark" <|
+      \() ->
+       Expect.true "Expected True when text has a line break mark" (postHasLineBreakMarking Fixtures.textWithLineBreakMark)
+    , test "Should return False when text hasn't a line break mark" <|
+      \() ->
+        Expect.false "Expected False when text hasn't a line brak mark" (postHasLineBreakMarking Fixtures.textWithoutLineBreakMark)
     ]
